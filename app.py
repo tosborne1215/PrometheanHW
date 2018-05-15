540a93ccce9351ad5bf33c7a9155521d8ed424f0
tosborne <tosborne1215@gmail.com>
2018-05-15 09:19:05 -0700

    Added unit tests for 3 classes: Manager, Worker, Grapher

--- app.py
+++ app.py
@@ -3,6 +3,51 @@
 from multiprocessing import Process, Queue, Pool
 
 
+class FileSearchManager(object):
+
+    def __init__(self):
+        pass
+
+    def find(self, pattern):
+        pass
+
+    def create_pool(self):
+        pass
+
+    def create_queue_from_dir(self):
+        pass
+
+    def get_results_from_worker(self):
+        pass
+
+    def check_file_exists(self, file_name):
+        pass
+
+    def compile_expression(self, expression):
+        pass
+
+    def get_results():
+        pass
+
+
+class FileSearchWorker(object):
+
+    def __init__(self):
+        pass
+
+    def search_file(self, file_name):
+        pass
+
+
+class GraphIt(object):
+
+    def __init__(self):
+        pass
+
+    def build_graph(self, data):
+        pass
+
+
 if __name__ == '__main__':
     subprocesses = list()
     filesQueue = Queue()
--- app_test.py
+++ app_test.py
@@ -1,13 +1,83 @@
 #!/usr/bin/python2.7
 
 import unittest
+import os
+import sys
+from app import FileSearchManager, FileSearchWorker, GraphIt
 
 
-class AppTest(unittest.TestCase):
+class FileSearchManagerTest(unittest.TestCase):
+    manager = None
+    data_folder = None
+
     def setUp(self):
-        pass
+        self.manager = FileSearchManager()
+        self.data_folder = root_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "data")
 
     def tearDown(self):
+        pass
+
+    def test_invalid_input():
+        pass
+
+    def test_valid_input():
+        pass
+
+    def test_create_pool():
+        pass
+
+    def test_create_queue():
+        pass
+
+    def test_create_queue_from_no_dir(self):
+        pass
+
+    def test_create_queue_from_too_large_dir(self):
+        pass
+
+    def test_create_queue_from_too_long_fname(self):
+        pass
+
+    def test_check_file_exists(self):
+        pass
+
+    def test_check_file_not_exists(self):
+        pass
+
+    def test_find_on_deep_dir(self):
+        pass
+
+    def test_find_in_many_files(self):
+        pass
+
+    def test_get_results(self):
+        pass
+
+
+class FileSearchWorkerTest(unittest.TestCase):
+
+    def test_search_large_file(self):
+        pass
+
+    # technically it is checked before now,
+    # but maybe it should check it anyway?
+    def test_search_nonexistent_file(self):
+        pass
+
+    def test_search_fname_too_long(self):
+        pass
+
+
+class GraphItTest(unittest.TestCase):
+
+    def test_build_graph(self):
+        pass
+
+    def test_build_graph_from_none_data(self):
+        pass
+
+    def test_build_graph_from_large_data(self):
         pass
 
 
